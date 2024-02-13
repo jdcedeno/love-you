60e93dc9fbb51a4916700640dc2033021b9c9e5f
JDCedeno <josedcedenocode@gmail.com>
2024-02-13 14:35:14 -0500

    love-you-01 clean up files

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
 import "./App.css"
 import React from "react"
-import { Canvas } from "@react-three/fiber"
-import {
-	Backdrop,
-	CameraControls,
-	Float,
-	RandomizedLight,
-	SpotLight,
-} from "@react-three/drei"
-import Valentin from "./components/Valentin/Valentin"
 
 const App = () => {
-	return (
-		<>
-			<Canvas>
-				<CameraControls />
-
-				<pointLight
-					distance={100}
-					intensity={800}
-					position={[0, 10, 10]}
-				/>
-
-				<Valentin scale={1} rotation={[0, 0, 0]} position={[0, 0, 0]} />
-			</Canvas>
-		</>
-	)
+	return <div id="app-container">0-you</div>
 }
 
 export default App
